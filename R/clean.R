cf493d9fdc0e57113cbfa99bb0810014730cf10b
Lin Quan <linquan726@gmail.com>
2022-04-25 16:03:57 -0500

    update

--- R/clean.R
+++ R/clean.R
@@ -5,11 +5,11 @@
 #' @examples
 #' dataclean("https://www.ers.usda.gov/webdocs/DataFiles/48747/Unemployment.csv")
 #' @author Zirou Zhou
-#' @import tidyverse
+#' @import tidyverse tidyr
 dataclean<- function(url){
 file=read.csv(url)
 file<-separate(file,Attribute,c("Attribute","year"),sep = -4)
-file<-separate(file,Area_name,c("Area_name","state"),sep = ",")
+file<-separate(file,Area_name,c("Area_name","state"),sep = ",", fill = "right")
 file$Area_name <- gsub(" County","", file$Area_name)
 file$year=as.numeric(file$year)
 return(file)
